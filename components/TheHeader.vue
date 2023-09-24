65141410598af8ad487cd947577c8817dbb53d06
Daniel Oaks <daniel@danieloaks.net>
2023-09-24 23:50:13 +1000

    Slightly improve header at small widths

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,27 +2,34 @@
   <nav
     class="sticky left-0 top-0 z-20 w-full bg-primary-500 dark:bg-primary-600"
   >
-    <div
-      class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4"
-    >
+    <div class="mx-auto flex max-w-screen-xl items-center justify-between p-4">
       <nuxt-link to="/" class="flex items-center">
-        <img src="/logo.png" class="mr-3 h-8" alt="MLP Drawing School logo" />
+        <img
+          src="/logo.png"
+          class="relative bottom-1 mr-3 h-8"
+          alt="MLP Drawing School logo"
+        />
         <span
           class="self-center whitespace-nowrap text-2xl font-semibold text-white"
         >
           MLPDS
         </span>
       </nuxt-link>
-      <div
-        id="navbar-sticky"
-        class="hidden w-full items-center justify-between md:flex md:w-auto"
-      >
+      <div class="flex w-auto items-center justify-center">
         <ul
-          class="mt-4 flex flex-col rounded-lg border p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0"
+          class="mx-4 flex flex-row flex-wrap items-center justify-center gap-4 rounded-lg border-0 font-medium"
         >
           <li>
-            <nuxt-link to="/guides/" class="header-link" aria-current="page">
-              Guides
+            <nuxt-link
+              to="/guides/"
+              class="header-link"
+              aria-current="page"
+              title="Drawing guides"
+            >
+              <div class="hidden md:block">Guides</div>
+              <div class="md:hidden">
+                <font-awesome-icon :icon="['fa', 'book']" />
+              </div>
             </nuxt-link>
           </li>
           <li>
@@ -30,13 +37,24 @@
               href="https://www.reddit.com/r/MLPdrawingschool/wiki/biweekly"
               target="_blank"
               class="header-link"
+              title="Drawing challanges"
             >
-              Challenges
+              <div class="hidden md:block">Challenges</div>
+              <div class="md:hidden">
+                <font-awesome-icon :icon="['fa', 'hat-cowboy-side']" />
+              </div>
             </a>
           </li>
           <li>
-            <nuxt-link to="/prompts" class="header-link">
-              Prompt Generator
+            <nuxt-link
+              to="/prompts"
+              class="header-link"
+              title="Drawing prompt generator"
+            >
+              <div class="hidden md:block">Prompt Generator</div>
+              <div class="md:hidden">
+                <font-awesome-icon :icon="['fa', 'dice']" />
+              </div>
             </nuxt-link>
           </li>
         </ul>
@@ -61,7 +79,7 @@ const colorMode = useColorMode();
 
 <style>
 .header-link {
-  @apply block rounded py-2 pl-3 pr-4 text-white transition-all md:bg-transparent md:p-0;
+  @apply block rounded bg-transparent p-0 text-white transition-all;
   &:hover,
   &.router-link-exact-active {
     @apply text-primary-100;
